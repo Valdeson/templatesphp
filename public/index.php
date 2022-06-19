813c5456f932174359b1890a71d85115d4d0e208
Valdeson <valdeson83@gmail.com>
2022-06-19 00:20:02 -0300

    How Output Buffer  Work

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "HOME";
+$data = ["name" => "Valdeson", "age" => 27];
+
+ob_start();
+
+extract($data);
+
+require 'home.php';
+
+$content = ob_get_contents();
+
+ob_end_clean();
+
+echo $content;
